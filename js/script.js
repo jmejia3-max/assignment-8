d7a9f51298c702be50bec3dbc013c5a3196be2f2
Kevin <kcarrillo@northpark.edu>
2026-03-22 23:50:07 -0500

    Update js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("script.js connected!");
+
+let answers = [];
+
+const questionBlock = document.querySelectorAll(".question-block");
+
+questionBlocks.forEach((block, index) => {
+    const buttons = block.querySelectorAll(".answer-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            buttons.forEach(btn => btn.classList.remove("selected"));
+
+            button.classList.add("selected");
+            
+            answers[index] = button.dataset.answer;
+
+            console.log(answers);
+        });
+    });
+});
